3f7821fbe03c69a6bb830ccf070d526c82dc02c6
Mateusz Erdman/Matek <mateusz.erdman@hotmail.com>
2019-05-12 12:49:09 +0100

    euklides interacja

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,26 @@
 package com.sda.algorytmy;
-
+import java.util.Scanner;
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner scannner = new Scanner(System.in);
+        int a = scannner.nextInt();
+        int b = scannner.nextInt();
+        int licznik = 0;
+
+        while (a != b){
+            if(a > b){
+                a = a- b;
+            }else{
+                b = b -a;
+            }
+            licznik++;
+        }
+        System.out.println("Liczba interacji: " +licznik);
+        System.out.println("NWD: " +a);
     }
+
 }
